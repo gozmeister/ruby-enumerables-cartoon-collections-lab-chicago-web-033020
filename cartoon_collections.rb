e6034c393e65719dd95983b1c39a39b09f4e9062
Sam Gorick <samgorick.sg@gmail.com>
2020-03-19 13:13:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(array)
   array.any? { |elem| elem.length > 4}
 end
 
-def find_the_cheese
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.include? { |elem| cheese_types }
 end
